f8bc826663aba68782f05392e4a08feb27b93a8f
xinhuixu <x.xu.1102@gmail.com>
2016-09-30 12:18:23 -0400

    update

--- app.py
+++ app.py
@@ -6,6 +6,16 @@ def login():
     print request_header
     return render_template('login.html')
 
-@app.route("/auth")
-def authenticate():
-    print
+@app.route("/auth/", methods=["POST"])
+def authenticate():    
+    username = "mangosteen"
+    password = "durian"
+    if request.method == "POST":
+        if request.form["username"]== username and request.form["password"] == password:
+            return "ok"
+        return "are you sure about this??"
+    else:
+        return "no......"
+        
+if __name__ == '__app__':
+    app.run(debug = True)
