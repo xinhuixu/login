a397beed347bc87ea443699b8a821478f7652eba
xinhuixu <x.xu.1102@gmail.com>
2016-10-03 19:41:34 -0400

    update usr check from csv

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, render_template
+import hashlib, csv
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -6,16 +8,37 @@ def login():
     return render_template('login.html')
 
 @app.route("/auth/", methods=["POST"])
-def authenticate():    
-    username = "mangosteen"
-    password = "durian"
+def authenticate():
+    form = request.form
     if request.method == "POST":
-        if request.form["username"]== username and request.form["password"] == password:
-            return render_template('auth.html',res="succ")
-        return render_template('auth.html',res="fail")
+        return render_template('auth.html',res=check(form["username"],form["password"]))
     else:
-        return "no......"
+        return "wyd??"
+
+def csvToDict(r):
+    d = {}
+    for row in r:
+        key = row[0]
+        val = row[1]
+        d[key] = val
+    return d
         
+    
+def check(username, password):
+    r = csv.reader(open("data/usr.csv","r"))
+    d = csvToDict(r)
+    if username in d:
+        if d[username] == password:
+            return "you did it. im so proud"
+        else:
+            return "wrong pw. good try"
+    else:
+        return "username not found. wyd? #take3sectoregister"
+    
+@app.route("/register/", methods=["POST"])
+def register():
+    return "0"
+    
 if __name__ == '__main__':
     app.debug = True
     app.run()
